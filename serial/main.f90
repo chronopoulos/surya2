618e38facd4d01c3df76e01aef05e212ab0a08f8
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-11-09 18:16:27 -0800

    serial/main.f90: output to run.dat

--- serial/main.f90
+++ serial/main.f90
@@ -21,10 +21,19 @@ program main
     t=0d0
     call initialize_fields()
     do iter=1,niter
-        print*, 'Timestep: ', iter
+
+        print*, 'Timestep: ', iter, ' of ', niter
         call advance_interior_a()
         call advance_interior_b()
         t=t+dt
+
+        if (iter/40*40 .eq. iter) then
+            open(95, file='run.dat', status='unknown', access='append')
+            write(95, '(6(e15.9,1x))') t, bfield(45,74), bfield(44,74), bfield(45,54), afield(120,122), afield(120,6)
+            close(95)
+        end if
+
+
     enddo ! iter
 
 end program main
