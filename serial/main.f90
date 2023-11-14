af93fcd00b605a0357f298e3811752b010685968
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-11-14 15:34:02 -0800

    main.f90: replace old run.dat

--- serial/main.f90
+++ serial/main.f90
@@ -20,18 +20,25 @@ program main
     ! Main Loop
     t=0d0
     call initialize_fields()
+    open(95, file=TRIM(ADJUSTL(data_dir))//"/run.dat", status='replace', action='write')
     do iter=1,niter
 
-        print*, 'Timestep: ', iter, ' of ', niter
         call advance_interior_a()
         call advance_interior_b()
         t=t+dt
 
         ! output
         if (MOD(iter, output_cadence) .eq. 0) then
-            open(95, file=TRIM(ADJUSTL(data_dir))//"/run.dat", status='unknown', access='append')
+            ! run.dat
+            if (iter .eq. 1) then
+                open(95, file=TRIM(ADJUSTL(data_dir))//"/run.dat", action='write', status='replace')
+            else
+                open(95, file=TRIM(ADJUSTL(data_dir))//"/run.dat", action='write', position='append')
+            end if
             write(95, '(6(e15.9,1x))') t, bfield(45,74), bfield(44,74), bfield(45,54), afield(120,122), afield(120,6)
             close(95)
+            ! stdout
+            print*, 'Timestep: ', iter, ' of ', niter
         end if
 
 
