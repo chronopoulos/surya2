43a77b1ebdeae5444ee265c1e5afd6881f34d25a
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-11-14 17:11:58 -0800

    add plot_dr.py

---
+++ serial/analysis/plot_dr.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+from PyQt5 import QtWidgets
+import pyqtgraph as pg
+
+import numpy as np
+from scipy.interpolate import griddata
+
+import os
+
+DATA_DIR = os.path.expanduser('~/data/surya2/serial')
+DATA_FILE = os.path.expanduser('diffrot.dat')
+
+N = 2 * 128 + 1
+RMIN = 0.382794500E+11
+RMAX = 0.695990000E+11
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, data):
+        super().__init__()
+
+        t =     data[:,0]
+        r =     data[:,1]
+        psi =   data[:,2]
+
+        x = r * np.sin(t)
+        y = RMAX - r*np.cos(t)
+
+        xf = np.linspace(0, RMAX, 2*N)
+        yf = np.linspace(0, 2*RMAX, 4*N)
+        xxf, yyf = np.meshgrid(xf, yf, indexing='ij')
+
+        psi_rect = griddata((x, y), psi, (xxf, yyf), method='linear')
+
+        self.imageView = pg.ImageView()
+        self.imageView.setImage(psi_rect)
+        self.setCentralWidget(self.imageView)
+
+        self.resize(600, 800)
+
+
+if __name__ == '__main__':
+
+    data = np.loadtxt(os.path.join(DATA_DIR, DATA_FILE))
+
+    app = QtWidgets.QApplication([])
+    main = MainWindow(data)
+    main.show()
+    app.exec()
